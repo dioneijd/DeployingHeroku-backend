f96559f3173fca44ec2be0714d874d8b068645b7
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-03-31 13:11:07 -0300

    added cors library

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 
 
